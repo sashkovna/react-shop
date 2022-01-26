8ff1678bf61cf82cc2797c47080fe1bee0939825
Anna <any9684@rambler.ru>
2022-01-26 15:55:16 +0300

    gh-pages add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ function Footer() {
         <div className="footer-copyright">
             <div className="container">
                 © {new Date().getFullYear()} Copyright Text
-                <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+                <a className="grey-text text-lighten-4 right"
+                   href='https://sashkovna.github.io/react-shop/'
+                   target='_blank'
+                   rel='noreferrer'
+                >Repo</a>
             </div>
         </div>
     </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 function Header() {
     return <nav className="green darken-1">
         <div className="nav-wrapper">
-            <a href="#" className="brand-logo">React Shop</a>
+            <a href="/" className="brand-logo">React Shop</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="!#">Repo</a></li>
+                <li><a
+                    href='https://sashkovna.github.io/react-shop/'
+                    target='_blank'
+                    rel='noreferrer'
+                >Repo</a></li>
             </ul>
         </div>
     </nav>
